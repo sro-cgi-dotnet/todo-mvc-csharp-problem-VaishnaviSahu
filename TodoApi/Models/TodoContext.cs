070537d6395c34383c4195b9ac89b60af0ffced0
VaishnaviSahu <vaishnavi.sahu@cgi.com>
2018-09-22 19:16:45 +0530

    todo_test

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models{
+    public class TodoContext : DbContext{
+        public DbSet<Note> Notes {get; set;}
+        public DbSet<CheckListItem> CheckLists {get; set;}
+        public DbSet<Label> Labels { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=dbnote;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Note>().HasMany(n => n.CheckList).WithOne().HasForeignKey(c => c.NoteId);
+            modelBuilder.Entity<Note>().HasMany(n => n.Labels).WithOne().HasForeignKey(c => c.NoteId);
+        }    
+    }
+}
---
+++ TodoTest/UnitTest1.cs
@@ -0,0 +1,87 @@
+using System;
+using Xunit;
+using Moq;
+using TodoApi.Models;
+using TodoApi.Controllers;
+using System.Collections.Generic;
+
+namespace TodoApi.Tests
+{
+    public class TodoApiTests
+    {
+
+        private List<Note> GetMockDatabase(){
+            return new List<Note>{
+                new Note{
+                    NoteId = 1,
+                    Title = "To do list",
+                    CheckList = new List<CheckListItem>{
+                        new CheckListItem{
+                            Id = 1,
+                            Text = "Visit Tirupathi",
+                            NoteId = 1
+                        }
+                    },
+                    Labels = new List<Label>{
+                        new Label{
+                            Id = 1,
+                            Name = "Hangout"
+                        }
+                    }
+                },
+                new Note{
+                    NoteId = 2,
+                    Title = "Trial",
+                    CheckList = new List<CheckListItem>{
+                        new CheckListItem{
+                            Id = 2,
+                            Text = "Try Xunit",
+                            NoteId = 2
+                        }
+                    },
+                    Labels = new List<Label>{
+                        new Label{
+                            Id = 2,
+                            Name = "Trial"
+                        }
+                    }
+                }
+            };
+        }
+
+        [Fact]
+        public void RetrieveAll_Positive()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = GetMockDatabase();
+            datarepo.Setup(d => d.GetAllNotes()).Returns(notes);
+            TodoController todoController = new TodoController(datarepo.Object);
+            var result = todoController.Get();
+            Assert.NotNull(result);
+            Assert.Equal(2 , notes.Count);
+        }
+
+         [Fact]
+        public void RetrieveById_negative()
+        {
+            var datarepo = new Mock<IDataRepo>();
+            List<Note> notes = GetMockDatabase();
+            int id = 3;
+            datarepo.Setup(d => d.GetNote(id)).Returns(notes.Find(n => n.NoteId == id));
+            TodoController todoController = new TodoController(datarepo.Object);
+            var result = todoController.Get(id);
+            Assert.Null(result.Value);
+        }
+        [Fact]
+             public void RetrieveById_positive(){
+             var datarepo = new Mock<IDataRepo>();
+             List<Note> notes = GetMockDatabase();
+             int id = 1;
+             datarepo.Setup(d => d.GetNote(id)).Returns(notes.Find(n => n.NoteId == id));
+             TodoController todoController = new TodoController(datarepo.Object);
+             var result = todoController.Get(id);
+             Assert.NotNull(result);
+             Assert.Equal(id, result.Value.NoteId);
+         }
+    }
+}
